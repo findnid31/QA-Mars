d3ff1d0a1108d633c3e7e66472228e6a86234d75
Nidhi Raju <NIR025@diyarme.com>
2024-09-18 21:09:00 +0300

    Onboarding task for Language and Skill

---
+++ Pages/Skill.cs
@@ -0,0 +1,107 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using QAMars.Utilities;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QAMars.Pages
+{
+    public class Skill : CommonDriver
+    {
+        public readonly IWebDriver driver;
+        public Skill(IWebDriver driver)
+        {
+            this.driver = driver;
+
+        }
+        public void AddSkill(IWebDriver driver, string skill, string level)
+        {
+
+            Wait.WaitToBeClickable(driver, "XPath", "//div[@data-tab='second']//div[text()='Add New']", 3);
+
+            //Click on Add skill button
+            IWebElement addSkillButton = driver.FindElement(By.XPath("//div[@data-tab='second']//div[text()='Add New']"));
+            addSkillButton.Click();
+
+            //Click on Add Skill Textbox
+            IWebElement addSkillTextbox = driver.FindElement(By.XPath("//input[@placeholder='Add Skill']"));
+            addSkillTextbox.SendKeys(skill);
+
+            //Choose skill level
+            IWebElement chooseSkillLevelDropdown = driver.FindElement(By.XPath("//select[@name='level']"));
+            chooseSkillLevelDropdown.SendKeys(level);
+            chooseSkillLevelDropdown.Click();
+
+            Wait.WaitToBeClickable(driver, "XPath", "//input[@value='Add']", 3);
+
+            //Click on Add button
+            IWebElement addButton = driver.FindElement(By.XPath("//input[@value='Add']"));
+            addButton.Click();
+            
+
+        }
+        public void ClearSkill(IWebDriver driver)
+        {
+            try
+            {
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                var deleteButtons = wait.Until(drv => drv.FindElements(By.XPath("//td[@class='right aligned']//i[@class='remove icon']")));
+
+                foreach (var button in deleteButtons)
+                {
+                    wait.Until(ExpectedConditions.ElementToBeClickable(button)).Click();
+                }
+            }
+            catch (NoSuchElementException)
+            {
+                Console.WriteLine("Nothing to delete");
+            }
+            catch (WebDriverTimeoutException)
+            {
+                Console.WriteLine("Timed out waiting for elements to be clickable");
+            }
+        }
+        public string GetNewSkill(IWebDriver driver, string skill)
+        {
+            Wait.WaitToBeVisible(driver, "XPath", "(//div[@data-tab='second']//tbody//td[1])[last()]", 3);
+            IWebElement newSkill = driver.FindElement(By.XPath("(//div[@data-tab='second']//tbody//td[1])[last()]"));
+            return newSkill.Text;
+        }
+
+        public void EditSkillRecord(IWebDriver driver, string updatedskill, string updatedlevel)
+        {
+            Thread.Sleep(4000);
+
+            //Click Edit button
+            IWebElement editSkillButton = driver.FindElement(By.XPath("(//div[@data-tab='second']//i[@class='outline write icon'])[1]"));
+            editSkillButton.Click();
+
+            //locate and update the value to be edited
+            IWebElement SkillToBeEdited = driver.FindElement(By.XPath("//input[@placeholder='Add Skill']"));
+            SkillToBeEdited.Clear();
+            SkillToBeEdited.SendKeys(updatedskill);
+
+            //Edit the skill level
+            IWebElement chooseSkillLevelDropdown = driver.FindElement(By.XPath("//select[@name='level']"));
+            chooseSkillLevelDropdown.SendKeys(updatedlevel);
+            
+            //Click on Update button
+            IWebElement updateButton = driver.FindElement(By.XPath("//input[@value='Update']"));
+            updateButton.Click();
+
+        }
+
+        public void DeleteSkillRecord(IWebDriver driver)
+        {
+            Thread.Sleep(4000);
+
+            IWebElement deleteButton = driver.FindElement(By.XPath("(//div[@data-tab='second']//i[@class='remove icon'])"));
+            deleteButton.Click();
+
+        }
+    }
+}
---
+++ Specflow/SkillTest.feature
@@ -0,0 +1,51 @@
+﻿Feature:  Skill Tab Test scenarios
+
+As a Mars portal user 
+I would like to create,edit and delete skill records
+so that I can manage my skill profile successfully
+
+@PositiveScenario
+Scenario: create Skill record with data
+	Given I logged into Mars portal 
+	And I navigate to Skill tab
+	When I add a skill "<Skill>" and level "<Level>"
+	Then the new "<Skill>" record should be created successfully
+
+	Examples: 
+	| Skill			 | Level        |
+	| C#			 | Beginner     |
+	| Henna Artist	 | Intermediate |
+	| Home baker     | Expert       |
+	| Math           | Expert       |
+
+@NegativeScenario
+Scenario Outline: Error is displayed when a Invalid Skill record is created
+Given I logged into Mars portal
+And I navigate to Skill tab
+When I add an invalid skill "<Invalid Skill>" and invalid level "<Invalid Level>"
+Then the error message should be displayed successfully
+
+Examples: 
+| Invalid Skill | Invalid Level |
+|               | Intermediate  |
+| Nail Art      |               |
+|  Math         |  Expert       |
+
+
+	Scenario: To Edit Skill record with new data
+	Given I logged into Mars portal 
+	And I navigate to Skill tab
+	When I edit an existing skill "<Updated Skill>" and level "<Updated Level>"
+	Then the new "<Updated Skill>" record should be displayed successfully
+
+	Examples: 
+	| Updated Skill		| Updated Level     |
+	| Sewing			| Beginner			|
+	| Baking			| Expert			|
+
+
+	Scenario Outline: delete an existing Skill record 
+	Given I logged into Mars portal
+	And I navigate to Skill tab
+	When I have an existing skill record to delete
+	Then the skill record should be deleted successfully
